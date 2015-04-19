8b069c227b392de8a1b2718965e5b861da20494c
jseteny <jseteny@gmail.com>
2015-04-19 17:02:43 +0200

    - Family members instead of scientists - Increment/decrement with 1/5 - TODO implement logic for New/Edit buttons

--- leaderboard.js
+++ leaderboard.js
@@ -1,57 +1,57 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
+// Set up a collection to contain familyMember information. On the server,
+// it is backed by a MongoDB collection named "familyMembers".
 
-Players = new Mongo.Collection("players");
+FamilyMembers = new Mongo.Collection("familyMembers");
 
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
-    players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1 } });
+    familyMembers: function () {
+      return FamilyMembers.find({}, { sort: { weight: -1, name: 1 } });
     },
     selectedName: function () {
-      var player = Players.findOne(Session.get("selectedPlayer"));
-      return player && player.name;
+      var familyMember = FamilyMembers.findOne(Session.get("selectedFamilyMember"));
+      return familyMember && familyMember.name;
     }
   });
 
   Template.leaderboard.events({
     'click .minus1': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: -1}});
+      FamilyMembers.update(Session.get("selectedFamilyMember"), {$inc: {weight: -1}});
     },
     'click .minus5': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: -5}});
+      FamilyMembers.update(Session.get("selectedFamilyMember"), {$inc: {weight: -5}});
     },
     'click .plus5': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
+      FamilyMembers.update(Session.get("selectedFamilyMember"), {$inc: {weight: 5}});
     },
     'click .plus1': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 1}});
+      FamilyMembers.update(Session.get("selectedFamilyMember"), {$inc: {weight: 1}});
     }
   });
 
-  Template.player.helpers({
+  Template.familyMember.helpers({
     selected: function () {
-      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
+      return Session.equals("selectedFamilyMember", this._id) ? "selected" : '';
     }
   });
 
-  Template.player.events({
+  Template.familyMember.events({
     'click': function () {
-      Session.set("selectedPlayer", this._id);
+      Session.set("selectedFamilyMember", this._id);
     }
   });
 }
 
-// On server startup, create some players if the database is empty.
+// On server startup, create some familyMembers if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Players.find().count() === 0) {
+    if (FamilyMembers.find().count() === 0) {
       var names = ["Jancsi", "Erika", "Matyi",
                    "Panni", "Zsuzsi"];
       _.each(names, function (name) {
-        Players.insert({
+        FamilyMembers.insert({
           name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
+          weight: Math.floor(Random.fraction() * 10) * 5
         });
       });
     }
