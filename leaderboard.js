7342fe8beb9a68dbb106adf20d678f184619c8cb
jseteny <jseteny@gmail.com>
2015-05-25 14:15:25 +0200

    - Changed tab size to 4

--- leaderboard.js
+++ leaderboard.js
@@ -4,68 +4,68 @@
 FamilyMembers = new Mongo.Collection("familyMembers");
 
 function updateOrCreateWeight(id, value) {
-  var old = FamilyMembers.find(id).fetch()[0];
-  var newTime = new Date().getTime();
-  var seconds=1000;
-  var diff = newTime - old.time;
-  if(diff<20*seconds) {
-    console.log('update');
-    FamilyMembers.update(id, {$set: {weight: value}});
-    FamilyMembers.update(id, {$set: {time: newTime}});
-  }else{
-    console.log('insert');
-    FamilyMembers.insert({name: old.name, weight: value, time: newTime})
-  }
+    var old = FamilyMembers.find(id).fetch()[0];
+    var newTime = new Date().getTime();
+    var seconds = 1000;
+    var diff = newTime - old.time;
+    if (diff < 20 * seconds) {
+        console.log('update');
+        FamilyMembers.update(id, {$set: {weight: value}});
+        FamilyMembers.update(id, {$set: {time: newTime}});
+    } else {
+        console.log('insert');
+        FamilyMembers.insert({name: old.name, weight: value, time: newTime})
+    }
 }
 
 if (Meteor.isClient) {
-  Template.leaderboard.helpers({
-    familyMembers: function () {
-      return FamilyMembers.find({});
-    },
-    selectedName: function () {
-      var familyMember = FamilyMembers.findOne(Session.get("selectedFamilyMember"));
-      return familyMember && familyMember.name;
-    }
-  });
+    Template.leaderboard.helpers({
+        familyMembers: function () {
+            return FamilyMembers.find({});
+        },
+        selectedName: function () {
+            var familyMember = FamilyMembers.findOne(Session.get("selectedFamilyMember"));
+            return familyMember && familyMember.name;
+        }
+    });
 
-  Template.leaderboard.events({
+    Template.leaderboard.events({
 
-    // update the text of the item on keypress but throttle the event to ensure
-    // we don't flood the server with updates (handles the event at most once
-    // every 300ms)
-    'keyup input[type=text]': _.throttle(function(event) {
-      var id = this._id;
-      var value = event.target.value;
-        updateOrCreateWeight(id, value);
-    }, 300)
-  });
+        // update the text of the item on keypress but throttle the event to ensure
+        // we don't flood the server with updates (handles the event at most once
+        // every 300ms)
+        'keyup input[type=text]': _.throttle(function (event) {
+            var id = this._id;
+            var value = event.target.value;
+            updateOrCreateWeight(id, value);
+        }, 300)
+    });
 
-  Template.familyMember.helpers({
-    selected: function () {
-      return Session.equals("selectedFamilyMember", this._id) ? "selected" : '';
-    }
-  });
+    Template.familyMember.helpers({
+        selected: function () {
+            return Session.equals("selectedFamilyMember", this._id) ? "selected" : '';
+        }
+    });
 
-  Template.familyMember.events({
-    'click': function () {
-      Session.set("selectedFamilyMember", this._id);
-    }
-  });
+    Template.familyMember.events({
+        'click': function () {
+            Session.set("selectedFamilyMember", this._id);
+        }
+    });
 }
 
 // On server startup, create some familyMembers if the database is empty.
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (FamilyMembers.find().count() === 0) {
-      var names = ["Jancsi"];
-      _.each(names, function (name) {
-        FamilyMembers.insert({
-          name: name,
-          weight: Math.floor(Random.fraction() * 10) * 5,
-          time:0
-        });
-      });
-    }
-  });
+    Meteor.startup(function () {
+        if (FamilyMembers.find().count() === 0) {
+            var names = ["Jancsi"];
+            _.each(names, function (name) {
+                FamilyMembers.insert({
+                    name: name,
+                    weight: Math.floor(Random.fraction() * 10) * 5,
+                    time: 0
+                });
+            });
+        }
+    });
 }
